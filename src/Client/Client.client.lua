a31cf3a9210afa643f181af9cf8e84185207e274
CluelessD3v <cluelessdevmail@gmail.com>
2023-01-08 03:47:58 -0500

    Delete server and client modules

--- src/Client/Client.client.lua
+++
@@ -1,9 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local RemoteHub = require(ReplicatedStorage.RemoteHub)
-
-local Value = RemoteHub.Value
-
-
-Value.getValue("Money").Changed:Connect(function(property)
-    print(property) 
-end)
