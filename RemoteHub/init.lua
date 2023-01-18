a90d015156dc2e3efa8fd43c25c4e1d867f29566
CluelessD3v <cluelessdevmail@gmail.com>
2023-01-18 17:50:22 -0500

    Re-wrote value to be just a value constructor, it no longer have a client part

--- RemoteHub/init.lua
+++ RemoteHub/init.lua
@@ -3,10 +3,9 @@ local RunService = game:GetService("RunService")
 local RemoteHub = {}
 if RunService:IsServer() then
     RemoteHub.Event = require(script.Event.EventServer)
-    RemoteHub.Value = require(script.Value.ValueServer)
+    RemoteHub.Value = require(script.Value)
 else
     RemoteHub.Event = require(script.Event.EventClient)
-    RemoteHub.Value = require(script.Value.ValueClient)
 end
 
 
